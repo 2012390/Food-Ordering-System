2fe12927b6e0ecd2cf6a3aa39ce915c3ee76bf14
2012390 <bscs2012390@szabist.pk>
2021-05-05 00:06:57 +0500

    make the codes more clear

--- FoodOrderingSystem/src/Menu.java
+++ FoodOrderingSystem/src/Menu.java
@@ -127,10 +127,7 @@ public class Menu extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-         dispose();
-       Customer customer=new Customer();
-      customer.setVisible(true);
+        // TODO add your handling code 
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
